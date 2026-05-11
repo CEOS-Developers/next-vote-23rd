1c31d1327b40f88cca7a13fb32d95814f1ca5156
YuminPark <p353700@gmail.com>
2026-05-12 07:24:27 +0900

    #3 [CHORE] 홈페이지 제목 반응형

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 const page = () => {
-  return <div>Ditda</div>;
+  return (
+    <div>
+      <h1 className="text-heading1-sb text-black md:text-[40px] md:leading-[135%] md:font-semibold md:tracking-[-0.8px]">
+        🏆<span className="text-purple-60">2026</span> CEOS <br />
+        23RD AWARDS
+      </h1>
+    </div>
+  );
 };
 
 export default page;

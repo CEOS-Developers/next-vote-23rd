b5fc7d29212b13db935a589b50a79ba0df1881b4
YuminPark <p353700@gmail.com>
2026-05-13 05:28:01 +0900

    #5 [FEAT] 배경 그래픽 애니메이션 구현

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,15 @@ const page = () => {
     <div>
       <HomeFlower
         aria-hidden="true"
-        className="pointer-events-none absolute top-1/2 -left-55 size-120 -translate-y-1/2 md:-top-10 md:-left-95 md:h-225 md:w-205 md:translate-y-0"
+        className="animate-fade-in-left pointer-events-none absolute top-1/2 -left-55 size-120 -translate-y-1/2 md:-top-10 md:-left-95 md:h-225 md:w-205 md:translate-y-0"
       />
       <HomeDiamond
         aria-hidden="true"
-        className="pointer-events-none absolute -top-20 -right-30 size-100 md:-top-25 md:-right-50 md:h-155 md:w-180"
+        className="animate-fade-in-diagonal pointer-events-none absolute -top-20 -right-30 size-100 md:-top-25 md:-right-50 md:h-155 md:w-180"
       />
       <HomeCircle
         aria-hidden="true"
-        className="pointer-events-none absolute -right-20 -bottom-40 size-100 md:right-auto md:-bottom-55 md:left-3/5 md:h-122 md:w-122 md:-translate-x-1/2"
+        className="animate-fade-in-bottom pointer-events-none absolute -right-20 -bottom-40 size-100 md:right-auto md:-bottom-55 md:left-3/5 md:h-122 md:w-122 md:-translate-x-1/2"
       />
       <div className="flex flex-col items-center gap-6">
         <h1 className="text-[40px] font-semibold text-black md:text-[50px] md:leading-[135%] md:font-semibold md:tracking-[-0.8px]">

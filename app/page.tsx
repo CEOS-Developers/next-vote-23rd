d9d987d7874f475e373d70597c06cdac36582801
YuminPark <p353700@gmail.com>
2026-05-13 05:35:14 +0900

    #5 [REFACTOR] 배경 그래픽 및 투표 카테고리 컴포넌트로 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,12 @@
-import HomeCircle from "@/app/assets/home/home_circle.svg";
-import HomeDiamond from "@/app/assets/home/home_diamond.svg";
-import HomeFlower from "@/app/assets/home/home_flower.svg";
 import Chip from "@/app/components/common/Chip";
 import CTA from "@/app/components/common/CTA";
+import BackgroundGraphic from "@/app/components/home/BackgroundGraphic";
+import { VOTE_CATEGORIES } from "@/app/constants/vote";
 
 const page = () => {
   return (
     <div>
-      <HomeFlower
-        aria-hidden="true"
-        className="animate-fade-in-left pointer-events-none absolute top-1/2 -left-55 size-120 -translate-y-1/2 md:-top-10 md:-left-95 md:h-225 md:w-205 md:translate-y-0"
-      />
-      <HomeDiamond
-        aria-hidden="true"
-        className="animate-fade-in-diagonal pointer-events-none absolute -top-20 -right-30 size-100 md:-top-25 md:-right-50 md:h-155 md:w-180"
-      />
-      <HomeCircle
-        aria-hidden="true"
-        className="animate-fade-in-bottom pointer-events-none absolute -right-20 -bottom-40 size-100 md:right-auto md:-bottom-55 md:left-3/5 md:h-122 md:w-122 md:-translate-x-1/2"
-      />
+      <BackgroundGraphic />
       <div className="flex flex-col items-center gap-6">
         <h1 className="text-[40px] font-semibold text-black md:text-[50px] md:leading-[135%] md:font-semibold md:tracking-[-0.8px]">
           🏆<span className="text-purple-60">2026</span> CEOS
@@ -27,9 +15,9 @@ const page = () => {
         </h1>
         <Chip label="투표 분야" />
         <div className="text-body2-sm md:text-heading2-m flex flex-col gap-3 pb-30 text-left">
-          <p># 프론트엔드 파트장 투표</p>
-          <p># 백엔드 파트장 투표</p>
-          <p># 데모데이 파트장 투표</p>
+          {VOTE_CATEGORIES.map(category => (
+            <p key={category}># {category}</p>
+          ))}
         </div>
         <CTA label="투표하러 가기" />
       </div>

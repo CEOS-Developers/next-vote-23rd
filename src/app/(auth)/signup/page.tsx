17778de0b063694b3a56c829a5fc5bdcdcab0842
minseo <kimminseo4216@gmail.com>
2026-05-14 11:44:13 +0900

    feat: 회원가입 페이지 추가

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+  return (
+    <main className="flex flex-col gap-3.5 w-[471px]">
+      <p className="text-[20px] font-extrabold leading-[135%] tracking-[-0.02px] py-3 border-b mb-5">
+        SIGNUP
+      </p>
+    </main>
+  );
+}
